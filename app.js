b2958765c55f4a46f65bfffd7c0f1d832f58a021
Katie Hoesley <katiehoesley@gJeffLent.local>
2020-05-20 15:04:19 -0600

    added code forstudents

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const users = require('./users.json')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => res.send(users))
+
+app.get('/:id', (req, res) => {
+    /* GET a user by their id */
+})
+
+app.post('/', (req, res) => {
+    /* POST user data using the request body */
+})
+
+app.get('/?name=?', (req, res) => {
+    /* GET a user by their name */
+})
+
+const port = 3000
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
