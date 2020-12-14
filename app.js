0059a49d1887bf3b01c79343cd591414806fdaf5
James-Kelley-Galvanize <james.kelley@galvanize.com>
2020-12-14 09:45:38 -0700

    Refactor slightly to parallel Learn edit - students will refactor first route, avoiding a route conflict

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send(users))
+   
+app.get('/', (req, res) => {
+    /* Refactor to GET a user by their name using query params *IF* it's passed.
+    If no name is passed in query params, should still behave as it currently does */
+    res.send(users)
+})
 
 app.get('/:id', (req, res) => {
     /* GET a user by their id */
@@ -16,9 +21,7 @@ app.post('/', (req, res) => {
     /* POST user data using the request body */
 })
 
-app.get('/', (req, res) => {
-    /* GET a user by their name using query params*/
-})
+
 
 const port = 3000
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
